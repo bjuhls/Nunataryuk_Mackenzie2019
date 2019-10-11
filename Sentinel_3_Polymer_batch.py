904d1f9a16712d12c1830d4fe247065e2a413369
schckngs <andreahilborn@gmail.com>
2019-10-11 14:58:53 -0300

    Update Sentinel_3_Polymer_batch.py

--- Sentinel_3_Polymer_batch.py
+++ Sentinel_3_Polymer_batch.py
@@ -7,6 +7,4 @@ s3_l1_filedir='<Your_path>/L1/S3*.SEN3'
 s3_l2_outdir='<Your_path>/L2_POLYMER/'
 
 for filename in glob(s3_l1_filedir):
-    print(filename)
-    print(s3_l2_outdir)
     run_atm_corr(Level1_OLCI(filename), Level2_NETCDF(outdir=s3_l2_outdir))
